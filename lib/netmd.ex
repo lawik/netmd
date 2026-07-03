8d539fed871abcc10d61324b475bfed25cb86663
Lars Wikman <lars@underjord.io>
2026-07-03 18:29:55 +0200

    🔥 initial commit 🔥

---
+++ lib/netmd.ex
@@ -0,0 +1,18 @@
+defmodule Netmd do
+  @moduledoc """
+  Documentation for `Netmd`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Netmd.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/netmd_test.exs
@@ -0,0 +1,8 @@
+defmodule NetmdTest do
+  use ExUnit.Case
+  doctest Netmd
+
+  test "greets the world" do
+    assert Netmd.hello() == :world
+  end
+end
